227e25193f77609a4a432beb330436c688620509
Bintang Ginanjar <bintang.ginanjar@gmail.com>
2021-06-17 11:39:01 +0700

    Parse book detail and output the result into dict

--- book/items.py
+++ book/items.py
@@ -5,8 +5,21 @@
 
 import scrapy
 
+from scrapy.item import Item, Field
+from itemloaders.processors import MapCompose, TakeFirst, Join
 
 class BookItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    # name = scrapy.Field()    
+    author = Field()
+    title = Field()
+    language = Field()
+    subject = Field()
+    category = Field()
+    ebookNo = Field()
+    releaseDate = Field()
+    copyrightStatus = Field()
+    downloadNum = Field()
+    price = Field()
+    
     pass
--- book/spiders/book-spider.py
+++ book/spiders/book-spider.py
@@ -1,11 +1,33 @@
+import book
 import scrapy
 import string
 
 from scrapy.loader import ItemLoader
-#from nama.items import NamaItem
+from scrapy.item import Item, Field
+#from book.items import BookItem
+
+class TableItem(Item):
+    row = Field()
 
 class BookSpider(scrapy.Spider):
     name = 'book'
+
+    '''
+    custom_settings = {
+        'FEED_EXPORT_FIELDS': [
+            'author',
+            'title',
+            'language',
+            'subject',
+            'category',
+            'ebookNo',
+            'releaseDate',
+            'copyrightStatus',
+            'downloadNum',
+            'price'
+        ]
+    }
+    '''
 
     def start_requests(self):
 
@@ -16,3 +38,33 @@ class BookSpider(scrapy.Spider):
         yield prodResponse
 
     def parse(self, response):
+
+        for row in response.css('div.bookshelf_pages > ul > li'):
+            shelfUrl = row.css('a::attr(href)').get()
+            yield response.follow(shelfUrl, self.parseShelf)           
+
+    def parseShelf(self, response):
+        for row in response.css('li.booklink'):
+            bookUrl = row.css('a::attr(href)').get()
+
+            yield response.follow(bookUrl, self.parseBook)
+
+        #for nav in response.css('div.padded > span.links'):
+
+
+    def parseBook(self, response):
+        item = TableItem()
+
+        tempDict = dict()
+
+        for row in response.xpath('//table[@class="bibrec"]//tr'):
+            #print(row.css('td > a::text').get())
+            
+            if (row.css('td > a::text').get()):
+                tempDict[row.xpath('th//text()').get()] = row.css('td > a::text').get()
+            else:
+                tempDict[row.xpath('th//text()').get()] = row.xpath('td//text()').get()                    
+            
+            item['row'] = tempDict
+
+        return item
